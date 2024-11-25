2848775e957cef9ec560f07e3f03cf267e398823
SpiritFoxo <37273399+SpiritFoxo@users.noreply.github.com>
2024-11-25 09:05:28 +0300

    11

--- js.test.js
+++ js.test.js
@@ -1,3 +1,5 @@
 const h = document.querySelector("h1");
 console.log(h.textContent);
 h.textContent = "Добро пожаловать на наш сайт!";
+const h2 = document.getElementById("frontend");
+h2.textContent.fontcolor
